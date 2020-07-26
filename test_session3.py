b7641d5b36af850340a3085352c2c868693df81d
Manjunath-Yelipeta <50134758+Manjunath-Yelipeta@users.noreply.github.com>
2020-07-26 22:31:36 +0530

    Add files via upload

--- test_session3.py
+++ test_session3.py
@@ -44,12 +44,14 @@ def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
 def test_readme_contents():
-    readme_words=[word for line in open('README.md', 'r') for word in line.split()]
+    readme = open("README.md", "r",encoding="utf-8")
+    readme_words = readme.read().split()
+    readme.close()
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 def test_readme_proper_description():
     READMELOOKSGOOD = True
-    f = open("README.md", "r")
+    f = open("README.md", "r",encoding="utf-8")
     content = f.read()
     f.close()
     for c in README_CONTENT_CHECK_FOR:
@@ -59,7 +61,7 @@ def test_readme_proper_description():
     assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
 
 def test_readme_file_for_formatting():
-    f = open("README.md", "r")
+    f = open("README.md", "r",encoding="utf-8")
     content = f.read()
     f.close()
     assert content.count("#") >= 10
